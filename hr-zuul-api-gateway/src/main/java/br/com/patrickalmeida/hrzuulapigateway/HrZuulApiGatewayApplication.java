30c7afae948da8cf65abec070afce857f50b15e8
Patrick Almeida <patricksalmeida@gmail.com>
2021-03-18 07:55:44 -0300

    Configuring zuul api gateway

--- hr-zuul-api-gateway/src/main/java/br/com/patrickalmeida/hrzuulapigateway/HrZuulApiGatewayApplication.java
+++ hr-zuul-api-gateway/src/main/java/br/com/patrickalmeida/hrzuulapigateway/HrZuulApiGatewayApplication.java
@@ -2,7 +2,11 @@ package br.com.patrickalmeida.hrzuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrZuulApiGatewayApplication {
 
