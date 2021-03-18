32ae043d08772c579c3af367f603949134d8a1a5
Patrick Almeida <patricksalmeida@gmail.com>
2021-03-17 21:33:54 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/patrickalmeida/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/patrickalmeida/hrpayroll/services/PaymentService.java
@@ -1,13 +1,34 @@
 package br.com.patrickalmeida.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.patrickalmeida.hrpayroll.entities.Payment;
+import br.com.patrickalmeida.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+		
+		String url = workerHost + "/workers/{id}";
+		
+		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
