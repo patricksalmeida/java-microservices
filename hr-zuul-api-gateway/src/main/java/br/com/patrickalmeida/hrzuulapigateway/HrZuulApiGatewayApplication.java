0e9ceebf051277f3b1d2bc2651ac1923484c371e
Patrick Almeida <patricksalmeida@gmail.com>
2021-03-18 07:48:58 -0300

    hr-zuul-api-gateway project created

---
+++ hr-zuul-api-gateway/src/main/java/br/com/patrickalmeida/hrzuulapigateway/HrZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrickalmeida.hrzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrZuulApiGatewayApplication.class, args);
+	}
+
+}
